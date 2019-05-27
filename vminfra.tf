e1757eca7148221f79ff4937740a553e39e9cdda
Prasannakumarreddy <40379893+PraSannaKuMarRedDdy@users.noreply.github.com>
2019-05-27 11:18:35 +0530

    Changed the location
    can you please try now

--- vminfra.tf
+++ vminfra.tf
@@ -1,6 +1,6 @@
 resource "azurerm_public_ip" "web" {
   name  = "web_1"
-  location = "Central US"
+  location = "${azurerm_resource_group.test.location}"
   resource_group_name     = "${azurerm_resource_group.test.name}"
   allocation_method       = "Dynamic"
 
